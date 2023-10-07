f055481c170e8a9909d4565067812edfa6b99e46
Lucas Rodriguez Davila <lucasrodriguezdavila@gmail.com>
2023-10-07 18:55:53 -0300

    added nnews

---
+++ utils/news/index.ts
@@ -0,0 +1,41 @@
+//https://news.google.com/rss/search?q=incendios+cordoba
+import convert from "xml-js";
+
+interface New {
+  title: {
+    _text: string;
+  };
+  link: {
+    _text: string;
+  };
+  guid: {
+    _attributes: {
+      isPermaLink: string;
+    };
+    _text: string;
+  };
+  pubDate: {
+    _text: string;
+  };
+  description: {
+    _text: string;
+  };
+  source: {
+    _attributes: {
+      url: string;
+    };
+    _text: string;
+  };
+}
+
+export const getNewsByQuery = async (query: string) => {
+  const response = await fetch(
+    `${window.location.origin}/api/news?query=${query}`,
+    {
+      mode: "no-cors",
+    }
+  );
+  const json = await response.json();
+  if (!json?.news?.length) return [];
+  return json as New;
+};
