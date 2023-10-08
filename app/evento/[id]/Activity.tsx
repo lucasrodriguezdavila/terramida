377471080e0b78a787e752a340707d984eb99bfb
Lucas Rodriguez Davila <lucasrodriguezdavila@gmail.com>
2023-10-08 10:23:55 -0300

    added verified comment

--- app/evento/[id]/Activity.tsx
+++ app/evento/[id]/Activity.tsx
@@ -67,7 +67,7 @@ const Activity: React.FC<Props> = ({}) => {
                 <>
                   <hr className="border-green-500" />
 
-                  <p>
+                  <div className="flex items-center gap-1">
                     Pertenece a{" "}
                     <Link
                       href={"/organizacion/" + comment.user.organization.id}
@@ -75,7 +75,21 @@ const Activity: React.FC<Props> = ({}) => {
                     >
                       {comment.user.organization.name}
                     </Link>
-                  </p>
+                    <span data-tooltip="Esta organizacion es goburnamental">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        className="w-5 h-5 ml-1"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M9.674 2.075a.75.75 0 01.652 0l7.25 3.5A.75.75 0 0117 6.957V16.5h.25a.75.75 0 010 1.5H2.75a.75.75 0 010-1.5H3V6.957a.75.75 0 01-.576-1.382l7.25-3.5zM11 6a1 1 0 11-2 0 1 1 0 012 0zM7.5 9.75a.75.75 0 00-1.5 0v5.5a.75.75 0 001.5 0v-5.5zm3.25 0a.75.75 0 00-1.5 0v5.5a.75.75 0 001.5 0v-5.5zm3.25 0a.75.75 0 00-1.5 0v5.5a.75.75 0 001.5 0v-5.5z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </span>
+                  </div>
                 </>
               )}
             </div>
