5fb45aeff58cb5c66ceea61d1f73d9dbaf7e9c65
Mauricio-Garcia-A <garcia.mauricio.ariel@gmail.com>
2023-10-08 14:26:06 -0300

    landingPage v0.1

---
+++ app/landingPage/page.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const items=[
+  {
+    title: '¿Qué hace Tenés fuego?',
+    subtitle: 'Trabajamos en conjunto con FIRMS (fire information for resource management system) un sistema de la NASA que monitorea posibles incendios forestales. Los satélites MODIS y VIIRS logran escanear la superficie terrestre cada 3 horas. De estos escaneos se generan píxeles sobre el mapa terráqueo, que representan anomalías térmicas (un punto donde la temperatura, el brillo y otros factores son considerados anormales y son relacionados a un incendio) determinadas por una serie de datos tratados algorítmicamente. Estos aparecen como cuadrados rojos dónde es posible, en mayor o menor medida, que un incendio se esté llevando a cabo al momento del escaneo.',
+    urlImg: './imagen1.png'
+  },
+  {
+    title: '¿Qué tan precisa es la plataforma?',
+    subtitle: 'El tamaño de estos incendios varía entre grandes, con áreas de hasta un kilómetro o hasta 50 metros en condiciones óptimas. Los píxeles sólo indican un área máxima, el fuego suele cubrir menos terreno y estar próximo al centro del recuadro rojo. Hay varias razones por las que MODIS o VIIRS pueden no haber detectado un incendio: el mismo puede haber comenzado y terminado entre observaciones satelitales; la cobertura de nubes, el humo denso o las copas de los árboles pueden oscurecer completamente un incendio; ocasionalmente los instrumentos no funcionan y no pueden observar nada durante estos momentos.',
+    urlImg: ''
+  },
+  {
+    title: 'Participá y colaborá ',
+    subtitle: 'En ¿Tenés fuego? podés dejar comentarios sobre los posibles focos de incendio. Puede que los satélites estén detectando una anomalía que corresponde en realidad una situación normal. Proponemos que vos, como ciudadano o institución, corrobores estos hechos y detalles si se tratan de incendios u otro tipo de evento. Los usuarios pueden dejar comentarios en las anomalías y otros, las instituciones corroboradas, pueden también confirmar o negar estos comentarios. Así, con el apoyo de la población, se podrá hacer un mapeo de la situación cercana de cada uno y complementar la información de rigor de la NASA con conocimiento local.',
+    urlImg: ''
+  }
+]
+
+
+
+export default function landingPage() {
+  return (
+    <>
+      <header style={{ backgroundImage: "url('./fondo1.png')" }}>
+      <div className="text-center flex flex-col items-center	 h-screen gap-6 p-40"
+       style={{backdropFilter:' blur(3px)'}}>
+        <h1 className=" text-3xl font-extrabold tracking-[-0.04em] text-black sm:text-5xl sm:leading-[3.5rem]">¿Tenés fuego?</h1>
+        <p className=' text-base text-gray-700  leading-7 text-slate-600s'>¿Tenés fuego? Es una plataforma de participación comunitaria donde colaboramos con información de la NASA para corroborar los datos de puntos calientes que reportan sus satélites.</p>
+        <button className='w-[300px] px-10 py-2 text-gray-200 bg-purple-600 rounded-full shadow-md text-lg hover:bg-gray-800 hover:border-red'>IR AL MAPA</button>
+      
+      </div>
+      </header>
+      <main>
+        {items.map(item => {
+          return <article className=' flex flex-col	justify-center alin-items-center gap-6 w-full min-h-screen flex flex-col justify-center items-center bg-blue-400 py-12 px-20 text-center'>
+                  <img src={item.urlImg} alt='img' />
+            <h2 className='text-lg font-semibold leading-6 text-slate-900'>{item.title}</h2>
+            <p className='mt-2 text-sm leading-6 text-slate-700'>{item.subtitle}</p>
+          </article>
+        })}
+      </main>
+      <footer className="w-full p-5 flex flex-col justify-center items-center bg-gray-800">
+        <p>ⓒ No-Copyright - Proyecto SpaceApps - TERRAMIDA</p>
+      </footer>
+    </>
+  )
+}
